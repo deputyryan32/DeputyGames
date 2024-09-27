9c7283d601ffa16489a1881be854cfce0b7bdceb
lilgodman210 <rybug5210@gmail.com>
2024-09-27 19:23:20 -0400

    Update

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -1,5 +1,5 @@
 document.getElementById('navbar-container').innerHTML = `
     <div class="navbar">
-        <a href="index.html" class="nav-link" id="home-link">Home</a>
+        <a href="index.html" class="nav-link ${window.location.pathname.endsWith('index.html') ? 'active' : ''}">Home</a>
     </div>
 `;
