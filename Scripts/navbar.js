d941f313a44749782c1fa119cae7fbff8fb78d6e
lilgodman210 <rybug5210@gmail.com>
2024-09-27 19:19:45 -0400

    Update navbar.js

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -1,25 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('navbar.html')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.text();
-    })
-    .then(data => {
-        document.getElementById('navbar-container').innerHTML = data;
-
-        // Highlight the active link
-        const currentPage = window.location.pathname.split("/").pop(); // Get the current page
-        const links = document.querySelectorAll('.nav-link');
-        
-        links.forEach(link => {
-            if (link.getAttribute('href') === currentPage) {
-                link.classList.add('active');
-            }
-        });
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-    });
-});
+document.getElementById('navbar-container').innerHTML = `
+    <div class="navbar">
+        <a href="index.html" class="nav-link" id="home-link">Home</a>
+    </div>
+`;
