33715f90d132e290d17503d286c56ac8ec096d0f
lilgodman210 <rybug5210@gmail.com>
2024-10-03 19:11:51 -0400

    V1

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// scripts.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const gamesList = document.getElementById("games");
+
+    // Simulated list of game folders (this would normally come from a server)
+    const games = [
+        { name: "Flappy Bird", path: "flappy_bird/index.html" }
+        // You can add more games here
+    ];
+
+    // Dynamically create list items for each game folder
+    games.forEach(game => {
+        const li = document.createElement("li");
+        li.innerHTML = `<a href="${game.path}">${game.name}</a>`;
+        gamesList.appendChild(li);
+    });
+});
