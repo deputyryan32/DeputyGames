5bb3eff3c7fb33777d8f93200c974c07cbe150c6
lilgodman210 <rybug5210@gmail.com>
2024-10-03 18:42:37 -0400

    DeputyGames

--- scripts/navbar.js
+++
@@ -1,7 +0,0 @@
-function openNavbar() {
-    document.getElementById('navbar').classList.add('active');
-}
-
-function closeNavbar() {
-    document.getElementById('navbar').classList.remove('active');
-}
